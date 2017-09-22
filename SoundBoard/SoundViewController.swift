8ff9ee075af4b1737dce985b8349f6c630d974e8
tf2polevaulter <connorjennison@me.com>
2017-09-22 12:46:42 -0700

    Initial Commit

---
+++ SoundBoard/SoundViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SoundViewController.swift
+//  SoundBoard
+//
+//  Created by Connor Jennison on 9/22/17.
+//  Copyright © 2017 Connor Jennison. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+
+    @IBAction func playTapped(_ sender: Any) {
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+  
+}
