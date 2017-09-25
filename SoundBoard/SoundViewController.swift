148959e4e4223247e5040c7bd088c286bf196100
tf2polevaulter <connorjennison@me.com>
2017-09-25 09:58:01 -0700

    Fully functioning soundboard app created. Create your own soundbites using the microphone and save/delete them in a list

--- SoundBoard/SoundViewController.swift
+++ SoundBoard/SoundViewController.swift
@@ -7,31 +7,104 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundViewController: UIViewController {
-
+    
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    
+    var audioRecorder: AVAudioRecorder?
+    var audioPlayer: AVAudioPlayer?
+    var audioURL: URL?
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        setUpRecorder()
+        playButton.isEnabled = false
+        addButton.isEnabled  = false
+        
     }
-
+    
+    func setUpRecorder()
+    {
+        do
+        {
+            //Create audio session
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            
+            
+            //Create settings for audio recorder
+            
+            var settings: [String:Any] = [:]
+            settings[AVFormatIDKey] = kAudioFormatMPEG4AAC
+            settings[AVSampleRateKey] = 44100.0
+            settings[AVNumberOfChannelsKey] = 2
+            
+            //Create URL for audio file
+            let basePath: String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [basePath, "audio.m4a"]
+            audioURL = NSURL.fileURL(withPathComponents: pathComponents)
+            
+            //Create audio recorder object
+            try audioRecorder = AVAudioRecorder(url: audioURL!, settings: settings)
+            audioRecorder!.prepareToRecord()
+        }
+        catch let error as NSError
+        {
+            print(error)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func recordTapped(_ sender: Any) {
+        if audioRecorder!.isRecording
+        {
+            audioRecorder!.stop()
+            recordButton.setTitle("Record", for: .normal)
+            playButton.isEnabled = true
+            addButton.isEnabled = true
+            
+        }
+        else
+        {
+            audioRecorder!.record()
+            recordButton.setTitle("Stop", for: .normal)
+        }
     }
-
+    
     @IBAction func playTapped(_ sender: Any) {
+        do
+        {
+        try audioPlayer = AVAudioPlayer(contentsOf: audioURL!)
+        audioPlayer?.play()
+        }
+        catch
+        {}
     }
-
+    
     @IBAction func addTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let sound = Sound(context: context)
+        sound.name = nameTextField.text
+        sound.audio = NSData(contentsOf: audioURL!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController?.popViewController(animated: true)
+        
     }
-  
+    
 }
--- SoundBoard/ViewController.swift
+++ SoundBoard/ViewController.swift
@@ -7,18 +7,82 @@
 //
 
 import UIKit
+import AVFoundation
+var audioPlayer: AVAudioPlayer?
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var sounds: [Sound] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate  = self
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do
+        {
+            try sounds = context.fetch(Sound.fetchRequest())
+            tableView.reloadData()
+        }
+        catch{}
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sounds.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let sound = sounds[indexPath.row]
+        cell.textLabel?.text = sound.name
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        do
+        {
+            let sound = sounds[indexPath.row]
+            try audioPlayer = AVAudioPlayer(data: sound.audio! as Data)
+            audioPlayer?.play()
+        }
+        catch{}
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete
+        {
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            
+            let sound = sounds[indexPath.row]
+            context.delete(sound)
+            
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            do
+            {
+                try sounds = context.fetch(Sound.fetchRequest())
+                tableView.reloadData()
+            }
+            catch{}
+            
+        }
+    }
+override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
